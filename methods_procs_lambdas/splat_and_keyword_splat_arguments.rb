d3a76dca44afd644e4b14894d82fb2330df05018
JoelLindow <joellindow@gmail.com>
2017-12-16 17:35:29 -0700

    optional arguments

--- methods_procs_lambdas/splat_and_keyword_splat_arguments.rb
+++ methods_procs_lambdas/splat_and_keyword_splat_arguments.rb
@@ -21,6 +21,8 @@ end
 roster 'A Guy', 'A Girl', 'A Banana with Legs', 'A Horse with a glove', 'Batman', 'Batboy', 'Athletic Chicken'
 
 # -----------------------------------
+puts "------------------------------"
+
 # THIS IS A KEY WORD ARGUMENT
 # This takes a hash value (key/value) stored data structure
 # It will iterate through it and let you use it how you want
@@ -40,3 +42,18 @@ player_roster = {
                 }
 
 roster_2(player_roster)
+
+# -------------------------------
+puts "------------------------------"
+# Optional Arguments
+# They also use a hash.
+# They give you an ability to pass any kind of argument you want to the method
+# then dynamically call it using the hash syntax
+
+def invoice options={}
+  puts options[:company]
+  puts options[:total]
+  puts options[:something_else]
+end
+
+invoice company: "Google", total: 100.0, something_else: "asdf"
